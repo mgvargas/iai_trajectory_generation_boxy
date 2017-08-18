10bbe7db262a34daa422cd0c36c8c6a5ef9b5300
Minerva Vargas <minervavargasg@gmail.com>
2017-08-18 15:02:47 +0200

    Correcting orientation error. Boxy reaches right orientation

---
+++ src/quaternion_operations.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# Copyright (c) 2017 Universitaet Bremen - Institute for Artificial Intelligence (Prof. Beetz)
+#
+# Author: Minerva Gabriela Vargas Gleason <minervavargasg@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import math
+import numpy as np
+
+
+def rotation_to_quaternion(rot_matrix):
+    w = math.sqrt(1 + rot_matrix[0, 0] + rot_matrix[1, 1] + rot_matrix[2, 2]) / 2
+    x = (rot_matrix[2, 1] - rot_matrix[1, 2]) / (4 * w)
+    y = (rot_matrix[0, 2] - rot_matrix[2, 0]) / (4 * w)
+    z = (rot_matrix[1, 0] - rot_matrix[0, 1]) / (4 * w)
+    return np.array([x, y, z, w])
+
+
+def quaternion_to_euler(q):
+    x, y, z, w = q
+    roll = math.atan2(2 * (w * x + y * z), (1 - 2 * (x * x - y * y)))
+    t2 = 2.0 * (w * y - z * x)
+    t2 = 1 if t2 > 1 else t2
+    t2 = -1 if t2 < -1 else t2
+    pitch = math.asin(t2)
+    t3 = 2 * (w * z + x * y)
+    t4 = 1.0 - 2 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return np.array([roll, pitch, yaw])
+
+
+def q_mult(q1, q2):
+    # Quaternion multiplication
+    x1, y1, z1, w1 = q1
+    x2, y2, z2, w2 = q2
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
+    z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
+    return np.array([x, y, z, w])
+
+
+def q_norm(q):
+    x, y, z, w = q
+    norm = math.pow(w, 2) + math.pow(x, 2) + math.pow(y, 2) + math.pow(z, 2)
+    return norm
+
+
+
+def q_inv(q1):
+    x1, y1, z1, w1 = q1
+    d = q_norm(q1)
+    w = w1 / d
+    x = -x1 / d
+    y = -y1 / d
+    z = -z1 / d
+    return np.array([x, y, z, w])
+
+
+def main():
+    print 'Some simple operations with quaternions'
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
--- src/sim_controller.py
+++ src/sim_controller.py
@@ -16,21 +16,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import math
 import numpy as np
 import sys
+from math import radians, pow, sqrt
 from qpoases import PyOptions as Options
 from qpoases import PyPrintLevel as PrintLevel
 from qpoases import PyReturnValue as returnValue
 from qpoases import PySQProblem as SQProblem
-
+import quaternion_operations as qo
 import PyKDL as kdl
 import actionlib
 # Plot for debugging
 import plotly.offline
 import plotly.graph_objs as go
 import rospy
-import tf2_ros
 from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import PoseArray, Pose
 from iai_naive_kinematics_sim.msg import ProjectionClock
@@ -53,8 +52,6 @@ class MoveToGPServer:
                                                     self.cancel_cb, auto_start=False)
         self.action_server.start()
         self.action_status = GoalStatus()
-        self.tfBuffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tfBuffer)
 
         # Variable definition
         self.grip_left = 'left_gripper_tool_frame'
@@ -67,7 +64,8 @@ class MoveToGPServer:
         self.joint_values_kdl = kdl.JntArray(self.nJoints)
         self.eef_pose = kdl.Frame()
         # TODO: find appropriate max acceleration
-        self.accel_max = np.array([0.3, 0.3, 0.3, 0.01, 0.64, 0.64, 0.75, 0.75, 0.75, 1.05, 1.05])
+        #self.accel_max = np.array([0.3, 0.3, 0.3, 0.01, 0.64, 0.64, 0.75, 0.75, 0.75, 1.05, 1.05])
+        self.accel_max = np.array([0.3, 0.3, 0.3, 0.02, 0.9, 0.9, 0.95, 0.95, 0.95, 1.05, 1.05])
 
         rospy.Subscriber('/joint_states', JointState, self.joint_callback)
         self.pub_clock = rospy.Publisher('/simulator/projection_clock', ProjectionClock, queue_size=3)
@@ -164,9 +162,9 @@ class MoveToGPServer:
     def generate_trajectory(self, time):
         if self.goal_received:
             self.get_urdf()
-            self.qpoases_config()
+            init_error_pos = self.qpoases_config()
             np.set_printoptions(suppress=True, precision=2, linewidth=180)
-            self.qpoases_calculation()
+            self.qpoases_calculation(init_error_pos)
 
             if self.success:
                 self._result.trajectory = self._feedback.sim_trajectory
@@ -176,6 +174,32 @@ class MoveToGPServer:
             self.active_goal_flag = False
             self.goal_received = False
         return 0
+
+    def calc_posit_error(self):
+        self.calc_eef_position(self.joint_values)
+        eef_posit = np.array([self.eef_pose.p[0], self.eef_pose.p[1], self.eef_pose.p[2]])
+        error_posit = (self.goal_posit - eef_posit) * self.prop_gain
+        limit_p = [abs(x/self.prop_gain) for x in error_posit]
+        return error_posit, limit_p
+
+    def calc_orient_error(self, eef_orient, goal_orient, thresh, limit_p):
+        if limit_p[2] >= 0.25:
+            error_orient = np.array([0.0, 0.0, 0.0])
+            rot_error = 1
+        else:
+            eef_inv = qo.q_inv(eef_orient)
+            rot_vector = qo.q_mult(eef_inv, goal_orient)
+            rot_error = sqrt(pow(rot_vector[0], 2) + pow(rot_vector[1], 2) + pow(rot_vector[2], 2))
+
+            if (thresh - rot_error) >= 0:
+                scaling = 1
+            else:
+                scaling = thresh/rot_error
+
+            slerp = quaternion_slerp(eef_orient, goal_orient, scaling)
+            quat_error = qo.q_mult(slerp, eef_inv)
+            error_orient = qo.quaternion_to_euler(quat_error)
+        return error_orient, rot_error
 
     def qpoases_config(self):
         # QPOases needs as config parameters:
@@ -206,21 +230,21 @@ class MoveToGPServer:
         self.calc_eef_position(self.joint_values)
         self.goal_quat = np.array([self.goal_pose.transform.rotation.x, self.goal_pose.transform.rotation.y,
                          self.goal_pose.transform.rotation.z, self.goal_pose.transform.rotation.w])
-        self.goal_orient = self.quaternion_to_euler(self.goal_quat)
-        self.error_orient = [0.0, 0.0, 0.0]
-        eef_orient_quat = self.rotation_to_quaternion(self.eef_pose.M)
-        limit_rot_z = abs(self.quaternion_to_euler(eef_orient_quat)[2] - self.goal_orient[2])
-        if limit_rot_z > math.radians(180):
+        self.goal_orient = qo.quaternion_to_euler(self.goal_quat)
+
+        eef_orient_quat = qo.rotation_to_quaternion(self.eef_pose.M)
+        limit_rot_z = abs(qo.quaternion_to_euler(eef_orient_quat)[2] - self.goal_orient[2])
+        if limit_rot_z > radians(180):
             print 'rotate goal'
-            rot = quaternion_from_euler(0,0,math.radians(180))
-            self.goal_orient = self.q_mult(self.goal_quat, rot)
-            self.goal_orient = self.quaternion_to_euler(self.goal_quat)
+            rot = quaternion_from_euler(0, 0, radians(180))
+            self.goal_quat  = qo.q_mult(self.goal_quat, rot)
+            self.goal_orient = qo.quaternion_to_euler(self.goal_quat)
+        error_orient = np.array([0.0, 0.0, 0.0])
 
         # Error in EEF position
         self.goal_posit = np.array([self.goal_pose.transform.translation.x, self.goal_pose.transform.translation.y,
                                self.goal_pose.transform.translation.z])
-        eef_posit = np.array([self.eef_pose.p[0], self.eef_pose.p[1], self.eef_pose.p[2]])
-        self.error_posit = (self.goal_posit - eef_posit) * self.prop_gain
+        error_posit, lim = self.calc_posit_error()
 
         # Get jacobian
         self.jacobian = self.get_jacobian()
@@ -239,10 +263,10 @@ class MoveToGPServer:
         self.joint_dist_upper_lim = np.empty([self.nJoints])
         self.joint_dist_lower_lim = np.subtract(self.joint_limits_lower, self.joint_values)
         self.joint_dist_upper_lim = np.subtract(self.joint_limits_upper, self.joint_values)
-        self.lbA = np.hstack((self.error_posit, self.error_orient, self.joint_dist_lower_lim, ac_lim_lower))
-        self.ubA = np.hstack((self.error_posit, self.error_orient, self.joint_dist_upper_lim, ac_lim_upper))
-        # self.lbA = np.hstack((self.error_posit, self.error_orient, self.joint_dist_lower_lim))
-        # self.ubA = np.hstack((self.error_posit, self.error_orient, self.joint_dist_upper_lim))
+        self.lbA = np.hstack((error_posit, error_orient, self.joint_dist_lower_lim, ac_lim_lower))
+        self.ubA = np.hstack((error_posit, error_orient, self.joint_dist_upper_lim, ac_lim_upper))
+        # self.lbA = np.hstack((error_posit, error_orient, self.joint_dist_lower_lim))
+        # self.ubA = np.hstack((error_posit, error_orient, self.joint_dist_upper_lim))
 
         # Create vector g
         self.g = np.zeros(self.nJoints+self.n_slack)
@@ -257,12 +281,15 @@ class MoveToGPServer:
         # Define matrix H
         self.H = np.diag(np.hstack((np.diag(self.jweights), self.sweights)))
         # print 'self.H ', np.shape(self.H), '\n',self.H
+        return error_posit
 
-    def qpoases_calculation(self):
+    def qpoases_calculation(self, error_posit):
+        error_orient = np.array([0.0, 0.0, 0.0])
         # Variable initialization
         self._feedback.sim_trajectory = [self.current_state]
-        limit_o = [abs(x) for x in self.error_orient]
-        limit_p = [abs(x) for x in self.error_posit]
+        # limit_o = [abs(x) for x in error_orient]
+        limit_o = 1
+        limit_p = [abs(x) for x in error_posit]
         eef_pose_array = PoseArray()
 
         # Setting up QProblem object
@@ -274,7 +301,7 @@ class MoveToGPServer:
         Opt = np.zeros(self.problem_size[1])
         i = 0
         precision = [0.03, 0.03, 0.03]
-        precision_o = [0.07, 0.07, 0.07]
+        precision_o = 0.02
 
         # Config iai_naive_kinematics_sim, send commands
         clock = ProjectionClock()
@@ -305,7 +332,8 @@ class MoveToGPServer:
             vel_p.append(np.array(Opt[x+3]))
             high.append(np.array([self.ubA[9+x]/2]))
 
-        while np.any(limit_p > precision) or np.any(limit_o > precision_o):
+        #while np.any(limit_p > precision) or np.any(limit_o > precision_o):
+        while np.any(limit_p > precision) or limit_o > precision_o:
             # tic = rospy.get_rostime()
             # Check if client cancelled goal
             if not self.active_goal_flag:
@@ -319,11 +347,11 @@ class MoveToGPServer:
             [ac_lim_lower, ac_lim_upper] = self.acceleration_limits(Opt)
             self.joint_dist_lower_lim = self.joint_limits_lower - self.joint_values
             self.joint_dist_upper_lim = self.joint_limits_upper - self.joint_values
-            self.lbA = np.hstack((self.error_posit, self.error_orient, self.joint_dist_lower_lim, ac_lim_lower))
-            self.ubA = np.hstack((self.error_posit, self.error_orient, self.joint_dist_upper_lim, ac_lim_upper))
+            self.lbA = np.hstack((error_posit, error_orient, self.joint_dist_lower_lim, ac_lim_lower))
+            self.ubA = np.hstack((error_posit, error_orient, self.joint_dist_upper_lim, ac_lim_upper))
 
             # Recalculate H matrix
-            self.calculate_weigths()
+            self.calculate_weigths(error_posit)
 
             vel_calculation = SQProblem(self.problem_size[1], self.problem_size[0])
             vel_calculation.setOptions(options)
@@ -344,23 +372,11 @@ class MoveToGPServer:
             velocity_msg.velocity[1] = Opt[1]
 
             # Recalculate Error in EEF position
-            self.calc_eef_position(self.joint_values)
-            eef_posit = np.array([self.eef_pose.p[0], self.eef_pose.p[1], self.eef_pose.p[2]])
-            self.error_posit = (self.goal_posit - eef_posit)*self.prop_gain
-            limit_p = abs(self.error_posit/self.prop_gain)
+            error_posit, limit_p = self.calc_posit_error()
 
             # Recalculate Error in EEF orientation
-            eef_orient_quat = self.rotation_to_quaternion(self.eef_pose.M)
-            q_interp = quaternion_slerp(eef_orient_quat, self.goal_quat, 0.3)
-            if limit_p[2] >= 0.00012:
-                self.error_orient = np.array([0.0, 0.0, 0.0])
-                limit_o = self.error_orient/self.prop_gain_orient
-            else:
-                q_interp = quaternion_slerp(eef_orient_quat, self.goal_quat, 0.3)
-                orient_error = self.quaternion_to_euler(q_interp) - self.quaternion_to_euler(eef_orient_quat)
-                # orient_error = self.quaternion_to_euler(q_interp)
-                self.error_orient = orient_error * self.prop_gain_orient
-                limit_o = abs(self.goal_orient - self.quaternion_to_euler(q_interp))
+            eef_orient = qo.rotation_to_quaternion(self.eef_pose.M)
+            error_orient, limit_o = self.calc_orient_error(eef_orient, self.goal_quat, 0.3, limit_p)
 
             # Print velocity for iai_naive_kinematics_sim
             velocity_msg.header.stamp = rospy.get_rostime()
@@ -379,10 +395,10 @@ class MoveToGPServer:
             eef_pose_msg.position.x = self.eef_pose.p[0]
             eef_pose_msg.position.y = self.eef_pose.p[1]
             eef_pose_msg.position.z = self.eef_pose.p[2]
-            eef_pose_msg.orientation.x = eef_orient_quat[0]
-            eef_pose_msg.orientation.y = eef_orient_quat[1]
-            eef_pose_msg.orientation.z = eef_orient_quat[2]
-            eef_pose_msg.orientation.w = eef_orient_quat[3]
+            eef_pose_msg.orientation.x = eef_orient[0]
+            eef_pose_msg.orientation.y = eef_orient[1]
+            eef_pose_msg.orientation.z = eef_orient[2]
+            eef_pose_msg.orientation.w = eef_orient[3]
             eef_pose_array.poses.append(eef_pose_msg)
 
             # Plot for debuging
@@ -390,17 +406,17 @@ class MoveToGPServer:
                 low[x] = np.hstack((low[x], self.lbA[9+x]/2))
                 pos[x] = np.hstack((pos[x], self.joint_values[x+3]))
                 vel_p[x] = np.hstack((vel_p[x], Opt[x+3]))
-                high[x] = np.hstack((high[x], self.ubA[9+x]/2))'''
+                high[x] = np.hstack((high[x], self.ubA[9+x]/2))
             for x in range(6):
                 if x < 3:
                     error[x] = np.hstack((error[x], self.lbA[x]/self.prop_gain))
                 else:
                     # error[x] = np.hstack((error[x], self.lbA[x]/self.prop_gain_orient))
-                    e = self.goal_orient - self.quaternion_to_euler(eef_orient_quat)
+                    e = self.goal_orient - qo.quaternion_to_euler(eef_orient_quat)
                     error[x] = np.hstack((error[x], e[x-3]))
             base_weight = np.hstack((base_weight, self.jweights[0, 0]))
             arm_weight = np.hstack((arm_weight, self.jweights[4, 4]))
-            triang_weight = np.hstack((triang_weight, self.jweights[3, 3]))
+            triang_weight = np.hstack((triang_weight, self.jweights[3, 3]))'''
             t = np.hstack((t, i))
 
             self.success = True
@@ -412,14 +428,16 @@ class MoveToGPServer:
             print '\n iter: ', i
             # toc = rospy.get_rostime()
             # print (toc.nsecs-tic.nsecs)/10e9, 'sec Elapsed'
-            '''print 'joint_vel: ', Opt[:-6]
+            print 'joint_vel: ', Opt[:-6]
             print 'slack    : ', Opt[-6:]
-            print 'eef_ori:   ', self.quaternion_to_euler(eef_orient_quat)
-            print 'ori error: ', self.error_orient/self.prop_gain_orient
-            # print 'ori error: ', self.goal_orient - self.quaternion_to_euler(eef_orient_quat)
+            print 'eef_ori:   ', qo.quaternion_to_euler(eef_orient)
+            print 'ori error: ', error_orient
+            # print 'ori error: ', self.goal_orient - qo.quaternion_to_euler(eef_orient_quat)
             print 'goal_ori:  ', self.goal_orient
-            print '\neef error:  ', eef_posit
-            print 'pos error:  ', self.error_posit/self.prop_gain
+            print 'limit', limit_p
+            print 'pos error:  ', error_posit/self.prop_gain
+            '''print '\neef error:  ', eef_posit
+            print 'pos error:  ', error_posit/self.prop_gain
             print 'goal error: ', self.goal_posit
             print 'lbA ', np.shape(self.lbA), '\n', self.lbA[6:]
             print 'ubA ', np.shape(self.ubA), '\n',self.ubA[6:]'''
@@ -468,14 +486,14 @@ class MoveToGPServer:
                       xaxis=dict(title='Iterations', autotick=False, dtick=25, gridwidth=3, ),
                       yaxis=dict(title='Error', gridwidth=3, ), )
         fig = dict(data=data, layout=layout)
-        plotly.offline.plot(fig, filename='html/error.html', image='png', image_filename='error.html')
+        plotly.offline.plot(fig, filename='html/error.html', image='png', image_filename='error')
 
         data = [t_base, t_arm, t_triang]
         layout = dict(title="Weighs.",
                       xaxis=dict(title='Iterations', autotick=False, dtick=25, gridwidth=3,),
                       yaxis=dict(title='Weights',gridwidth=3,),)
         fig = dict(data=data, layout=layout)
-        plotly.offline.plot(fig, filename='html/weights.html', image='png', image_filename='html/weights.html')'''
+        plotly.offline.plot(fig, filename='html/weights.html', image='png', image_filename='weights')'''
 
         return 0
 
@@ -556,7 +574,7 @@ class MoveToGPServer:
             pass
         else:
             rospy.logerr('Could not calculate forward kinematics')
-        return 0
+        return self.eef_pose
 
     def acceleration_limits(self,joint_vel):
         ac_lim_lower = np.empty(0)
@@ -568,39 +586,6 @@ class MoveToGPServer:
             ac_lim_lower = np.hstack((ac_lim_lower, ac_lower))
             ac_lim_upper = np.hstack((ac_lim_upper, ac_upper))
         return ac_lim_lower, ac_lim_upper
-
-    @staticmethod
-    def rotation_to_quaternion(rot_matrix):
-        # print 'w', 1 + rot_matrix[0, 0] + rot_matrix[1, 1] + rot_matrix[2, 2]
-        w = math.sqrt(1+rot_matrix[0, 0] + rot_matrix[1, 1] + rot_matrix[2, 2])/2
-        x = (rot_matrix[2, 1] - rot_matrix[1, 2])/(4*w)
-        y = (rot_matrix[0, 2] - rot_matrix[2, 0])/(4*w)
-        z = (rot_matrix[1, 0] - rot_matrix[0, 1])/(4*w)
-        return np.array([x, y, z, w])
-
-    @staticmethod
-    def quaternion_to_euler(q):
-        x, y, z, w = q
-        roll = math.atan2(2 * (w * x + y * z), (1 - 2 * (x * x - y * y)))
-        t2 = 2.0 * (w * y - z * x)
-        t2 = 1 if t2 > 1 else t2
-        t2 = -1 if t2 < -1 else t2
-        pitch = math.asin(t2)
-        t3 = 2 * (w * z + x * y)
-        t4 = 1.0 - 2 * (y * y + z * z)
-        yaw = math.atan2(t3, t4)
-        return np.array([roll, pitch, yaw])
-
-    @staticmethod
-    def q_mult(q1, q2):
-        # Quaternion multiplication
-        x1, y1, z1, w1 = q1
-        x2, y2, z2, w2 = q2
-        w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
-        x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
-        y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
-        z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
-        return np.array([x, y, z, w])
 
     def get_jacobian(self):
         # Obtain jacobian for the selected arm
@@ -615,7 +600,7 @@ class MoveToGPServer:
         jac_array = np.reshape(jac_array, (jacobian.rows(), jacobian.columns()))
         return jac_array
 
-    def calculate_weigths(self):
+    def calculate_weigths(self, error_posit):
         # Weight of an active/inactive joint
         active_joint = 1e-2
         inactive_joint = 10
@@ -629,17 +614,17 @@ class MoveToGPServer:
         size_jac = np.shape(self.jacobian)
 
         # Find bigger distance to goal (x or y)
-        dist = max(abs(self.error_posit[0]), abs(self.error_posit[1]))
+        dist = max(abs(error_posit[0]), abs(error_posit[1]))
 
         # If the robot is too far away, move only the base
         if dist >= b:
             # print 'far'
             jweights = np.ones(w_len)*inactive_joint
             jweights[3]-= 1
-            if abs(self.error_posit[0]) > b:
+            if abs(error_posit[0]) > b:
                 jweights[0] = active_joint
                 self.sweights[0] = inactive_joint
-            if abs(self.error_posit[1]) > b:
+            if abs(error_posit[1]) > b:
                 jweights[1] = active_joint
                 self.sweights[1] = inactive_joint
             # Arm not active
@@ -676,7 +661,7 @@ class MoveToGPServer:
             jweights[0] = inactive_joint
             jweights[1] = inactive_joint
             # Triangle base weight
-            jweights[3] = (active_joint -inactive_joint)*(abs(self.error_posit[2]) - 0.0)/(0.6 - 0.0) + inactive_joint
+            jweights[3] = (active_joint -inactive_joint)*(abs(error_posit[2]) - 0.0)/(0.6 - 0.0) + inactive_joint
             if jweights[3] < active_joint:
                 jweights[3] = active_joint
             # self.sweights = np.ones(len(self.sweights))*inactive_joint/2
